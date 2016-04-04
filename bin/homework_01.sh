5f421b14a3687fa03861b8c25f9835b8a0515d71
Daekyu Lee <dklee@inderpark.com>
2016-04-04 21:04:29 +0900

    homework

--- bin/homework_01.sh
+++ bin/homework_01.sh
@@ -32,3 +32,16 @@ echo -n "Submit config files... "
     diff -Nuar a b | \
     curl -T - $submit_url/$homework/$email/$hostname) && \
 rm -rf $tmpdir
+
+dagui@sojin:~]$ curl -O http://studyhard.yidigun.com/linux-webdev/bin/homework_01.sh
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100   763  100   763    0     0    137      0  0:00:05  0:00:05 --:--:--   189
+dagui@sojin:~]$ sh homework_01.sh
+Enter your e-mail: dklee@interpark.com
+It it correct? (dklee@interpark.com) [y/N] y
+Your hostname is sojin.
+Collecting config files..done.
+Submit config files... Thanks for your submission.
+dagui@sojin:~]$
+
